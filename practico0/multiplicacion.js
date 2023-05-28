a54e1700600de15cbc96f7a8a9c7626882b0ab59
PauDankeHub <magda.paezgi@gmail.com>
2023-05-28 19:10:27 -0300

    Agregué los archivos resta.js y multiplicacion.js

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,4 @@
+let num1 = 2;
+let num2 = 17;
+let num3 = 6;
+console.log ("El producto de num1*num2*num3 =" + (num1*num2*num3));
---
+++ practico0/resta.js
@@ -0,0 +1,4 @@
+let minuendo=10;
+let sustraendo=2;
+console.log ("El resultado de 10 - 2 =" + (minuendo-sustraendo));
+
