6443562ed759691f46614d8e0214412128220178
PauDankeHub <magda.paezgi@gmail.com>
2023-05-28 19:23:30 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,10 @@
+let base = 5;
+let altura = 3;
+console.log ("El area del rectangulo de base 5 cm y altura 3 cm es = " + (base*altura));
+
+/*let base = 12.5;
+let altura = 5.3;
+console.log ("El area del rectangulo de base 12,5 cm y altura 5,3 cm es = " + (base*altura));
+*/
+
+
---
+++ practico0/promedio3.js
@@ -0,0 +1,18 @@
+let num1 = 5;
+let num2 = 7;
+let num3 = 9;
+let promedio = (num1+num2+num3)/3;
+console.log (promedio);
+
+/*let num1 = -2;
+let num2 = -3;
+let num3 = -8;
+let promedio = (num1+num2+num3)/3;
+console.log (promedio);
+
+let num1 = -1.2;
+let num2 = 5.3;
+let num3 = -3.3;
+let promedio = (num1+num2+num3)/3;
+console.log (promedio);
+*/
